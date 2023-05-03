b90c9ff8128a226bb29ad5ae4049a5511a3799ba
Álvaro G <lvaro G@Alvaro>
2023-05-03 09:47:35 +0200

    Commit

---
+++ Proyecto1/src/paquete1/principal.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ejemplo de control de versiones con Git");
+		System.out.println("Segunda línea");
+
+	}
+
+}
